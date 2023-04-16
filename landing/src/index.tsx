1b40258c9c9fd1f98d76adcf4b47291d512f0614
Yun <indl1670@gmail.com>
2023-04-16 23:24:10 +0900

    🎈 Setting Recoil #S08P31E203-38

--- landing/src/index.tsx
+++ landing/src/index.tsx
@@ -4,14 +4,17 @@ import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes";
+import { RecoilRoot } from "recoil";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+  <RecoilRoot>
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  </RecoilRoot>
 );
 
 // If you want to start measuring performance in your app, pass a function
