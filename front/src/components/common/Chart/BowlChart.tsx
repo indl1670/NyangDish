12acd8d04eb72de2f2c8a161fd408bb728da2b28
Yun <indl1670@gmail.com>
2023-04-19 14:28:11 +0900

    🎈Dashboard Page - Create Common Component - Modify css - Arranged Components #S08P31E203-43

--- front/src/components/common/Chart/BowlChart.tsx
+++ front/src/components/common/Chart/BowlChart.tsx
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import DefaultDiv from "../DefaultDiv";
 import ApexCharts from "react-apexcharts";
 import { useRecoilState } from "recoil";
 import { darkModeState } from "../../../recoil/states/page";
+import Modal from "../Modal";
+import ModalContent from "./ModalContent";
 
 export default function BowlChart() {
   const isDark = useRecoilState(darkModeState)[0];
+  const [modalOpen, setModalOpen] = useState(false);
+  const [day, setDay] = useState("");
+  const [time, setTime] = useState(0);
+
   const generateData = (count: any, yrange: any) => {
     var i = 0;
     var series = [];
@@ -124,107 +130,128 @@ export default function BowlChart() {
     },
   ];
 
-  const handleChart = () => {
-    console.log("Chart Click");
+  const handleChart = (xaxis: number, yaxis: number) => {
+    const x = ["일", "월", "화", "수", "목", "금", "토"];
+    const y = [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24];
+    setDay(x[xaxis]);
+    setTime(y[yaxis]);
+
+    openModal();
+  };
+
+  const openModal = () => {
+    setModalOpen(true);
+  };
+
+  const closeModal = (e: React.MouseEvent) => {
+    e.stopPropagation();
+    setModalOpen(false);
   };
 
   return (
-    <DefaultDiv>
-      <div className="relative flex flex-col my-2 h-[98%] w-[950px] rounded-xl dark:bg-WebDarkBackground2">
-        <h1 className="m-5 text-[2rem] font-bold dark:text-white">
-          냥그릇 차트
-        </h1>
-        <div className="flex flex-col justify-center m-auto w-[90%] h-full mb-3">
-          <div className="h-[50%]">
-            <ApexCharts
-              series={series}
-              type="heatmap"
-              options={{
-                chart: {
-                  zoom: {
+    <>
+      <DefaultDiv>
+        <div className="relative flex flex-col my-2 h-[98%] w-[950px] rounded-xl dark:bg-WebDarkBackground2">
+          <h1 className="m-5 text-[2rem] font-bold dark:text-white">
+            냥그릇 차트
+          </h1>
+          <div className="flex flex-col justify-center m-auto w-[90%] h-full mb-3">
+            <div className="h-[50%]">
+              <ApexCharts
+                series={series}
+                type="heatmap"
+                options={{
+                  chart: {
+                    zoom: {
+                      enabled: false,
+                    },
+                    events: {
+                      dataPointSelection: (event, chartContext, config) => {
+                        handleChart(config.dataPointIndex, config.seriesIndex);
+                      },
+                    },
+                  },
+                  dataLabels: {
                     enabled: false,
                   },
-                  events: {
-                    click: handleChart,
-                  },
-                },
-                dataLabels: {
-                  enabled: false,
-                },
-                xaxis: {
-                  categories: ["일", "월", "화", "수", "목", "금", "토"],
-                  labels: {
-                    style: {
-                      colors: `${isDark ? "#FFFFFF" : "#000000"}`,
+                  xaxis: {
+                    categories: ["일", "월", "화", "수", "목", "금", "토"],
+                    labels: {
+                      style: {
+                        colors: `${isDark ? "#FFFFFF" : "#000000"}`,
+                      },
                     },
                   },
-                },
-                yaxis: {
-                  labels: {
-                    style: {
-                      colors: `${isDark ? "#FFFFFF" : "#000000"}`,
+                  yaxis: {
+                    labels: {
+                      style: {
+                        colors: `${isDark ? "#FFFFFF" : "#000000"}`,
+                      },
                     },
                   },
-                },
-                colors: [`${isDark ? "#29325B" : "#9FA9D8"}`],
-              }}
-              width={"100%"}
-              height={"100%"}
-            ></ApexCharts>
-            <h1 className="text-[1.2rem] font-semibold text-center dark:text-white">
-              방문 시간대
-            </h1>
-          </div>
-          <div className="h-[50%] mt-10">
-            <ApexCharts
-              type="line"
-              series={series2}
-              options={{
-                chart: {
-                  zoom: {
+                  colors: [`${isDark ? "#29325B" : "#9FA9D8"}`],
+                }}
+                width={"100%"}
+                height={"100%"}
+              ></ApexCharts>
+              <h1 className="text-[1.2rem] font-semibold text-center dark:text-white">
+                방문 시간대
+              </h1>
+            </div>
+            <div className="h-[50%] mt-10">
+              <ApexCharts
+                type="line"
+                series={series2}
+                options={{
+                  chart: {
+                    zoom: {
+                      enabled: false,
+                    },
+                    width: "100%",
+                    height: "10px",
+                  },
+                  dataLabels: {
                     enabled: false,
                   },
-                  width: "100%",
-                  height: "10px",
-                },
-                dataLabels: {
-                  enabled: false,
-                },
-                stroke: {
-                  curve: "straight",
-                },
-                colors: [`${isDark ? "#29325B" : "#9FA9D8"}`],
-                grid: {
-                  row: {
-                    colors: ["#f5f5f5f5", "transparent"],
-                    opacity: 0.5,
+                  stroke: {
+                    curve: "straight",
                   },
-                },
-                xaxis: {
-                  categories: ["일", "월", "화", "수", "목", "금", "토"],
-                  labels: {
-                    style: {
-                      colors: `${isDark ? "#FFFFFF" : "#000000"}`,
+                  colors: [`${isDark ? "#29325B" : "#9FA9D8"}`],
+                  grid: {
+                    row: {
+                      colors: ["#f5f5f5f5", "transparent"],
+                      opacity: 0.5,
                     },
                   },
-                },
-                yaxis: {
-                  labels: {
-                    style: {
-                      colors: `${isDark ? "#FFFFFF" : "#000000"}`,
+                  xaxis: {
+                    categories: ["일", "월", "화", "수", "목", "금", "토"],
+                    labels: {
+                      style: {
+                        colors: `${isDark ? "#FFFFFF" : "#000000"}`,
+                      },
                     },
                   },
-                },
-              }}
-              width={"100%"}
-              height={"80%"}
-            ></ApexCharts>
-            <h1 className="text-[1.2rem] mt-5 font-semibold text-center dark:text-white">
-              사료통 무게
-            </h1>
+                  yaxis: {
+                    labels: {
+                      style: {
+                        colors: `${isDark ? "#FFFFFF" : "#000000"}`,
+                      },
+                    },
+                  },
+                }}
+                width={"100%"}
+                height={"80%"}
+              ></ApexCharts>
+              <h1 className="text-[1.2rem] mt-5 font-semibold text-center dark:text-white">
+                사료통 무게
+              </h1>
+            </div>
           </div>
         </div>
-      </div>
-    </DefaultDiv>
+      </DefaultDiv>
+      <Modal open={modalOpen} close={closeModal} header="방문 시간대 상세">
+        <ModalContent day={day} time={time} />
+      </Modal>
+    </>
   );
 }
