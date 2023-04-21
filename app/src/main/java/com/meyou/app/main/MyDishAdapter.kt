8acb871c180ac752316547d64754bfe3a07fd298
이미현 <algus7531@naver.com>
2023-04-21 17:58:07 +0900

    🌴네비게이션 연결

---
+++ app/src/main/java/com/meyou/app/main/MyDishAdapter.kt
@@ -0,0 +1,42 @@
+package com.meyou.app.main
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.meyou.app.R
+
+class MyDishAdapter(val context: Context, val List : MutableList<ContentsMyDishList>) : RecyclerView.Adapter<MyDishAdapter.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyDishAdapter.ViewHolder {
+        var v = LayoutInflater.from(parent.context).inflate(R.layout.main_mydisy_list, parent, false)
+
+        return ViewHolder(v)
+    }
+
+    override fun onBindViewHolder(holder: MyDishAdapter.ViewHolder, position: Int) {
+        holder.bindItems(List[position])
+    }
+
+    override fun getItemCount(): Int {
+        return List.size
+    }
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+
+        fun bindItems(item : ContentsMyDishList){
+            val rv_img = itemView.findViewById<ImageView>(R.id.dishImage)
+            val rv_name = itemView.findViewById<TextView>(R.id.dishName)
+            val rv_address = itemView.findViewById<TextView>(R.id.dishAddressText)
+            rv_name.text = item.dishName
+            rv_address.text = item.dishAddress
+            Glide.with(context)
+                .load(item.dishProfileImagePath)
+                .into(rv_img)
+        }
+
+    }
+}
