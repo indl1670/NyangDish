f7c6ffa1ee8b9ec88882b5fe972ceca1f0fc1509
Yun <indl1670@gmail.com>
2023-04-27 15:46:05 +0900

    🎈Thema - Setting Main Layout - Create Header - DarkMode #S08P31E203-138

--- frontend/src/pages/Main.tsx
+++ frontend/src/pages/Main.tsx
@@ -1,5 +1,28 @@
 import React from "react";
+import { useRecoilState } from "recoil";
+import { categoryState } from "../recoil/page";
+import DashBoard from "./DashBoard";
+import User from "./User";
+import Dish from "./Dish";
+import Report from "./Report";
+import Chart from "./Chart";
 
 export default function Main() {
-  return <div>Main</div>;
+  const category = useRecoilState(categoryState)[0];
+
+  return (
+    <div>
+      {category[0] ? (
+        <DashBoard />
+      ) : category[1] ? (
+        <User />
+      ) : category[2] ? (
+        <Dish />
+      ) : category[3] ? (
+        <Report />
+      ) : (
+        <Chart />
+      )}
+    </div>
+  );
 }
