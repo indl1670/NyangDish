32a7b81ed4bc678f5e1880a1a5da6c9b8a9db681
이미현 <algus7531@naver.com>
2023-04-27 09:56:00 +0900

    🌴 중간 저장

---
+++ app/src/main/java/com/meyou/app/detailDish/tap2/DIshCat.kt
@@ -0,0 +1,12 @@
+package com.meyou.app.detailDish.tap2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.meyou.app.R
+
+class DIshCat : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dish_cat)
+    }
+}
---
+++ app/src/main/java/com/meyou/app/detailDish/tap2/DIshCatInfo.kt
@@ -0,0 +1,9 @@
+package com.meyou.app.detailDish.tap2
+
+data class DIshCatInfo(
+    val dishProfileImagePath : String = "",
+    val dishName : String = "",
+    val isTNR : Boolean = false,
+    val time : String = "",
+//    val dishId : Long = 0L,
+    )
