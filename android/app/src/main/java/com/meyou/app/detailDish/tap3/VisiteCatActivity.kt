16a8c7aad1eeb2f380ed0172ce99ba9260745214
이미현 <algus7531@naver.com>
2023-05-01 15:03:47 +0900

    🌴냥그릇 상세 탭 3개 api 연동 전 까지 완료

--- android/app/src/main/java/com/meyou/app/detailDish/tap3/VisiteCatActivity.kt
+++ android/app/src/main/java/com/meyou/app/detailDish/tap3/VisiteCatActivity.kt
@@ -1,4 +1,4 @@
-package com.meyou.app.detailDish.tap2
+package com.meyou.app.detailDish.tap3
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
@@ -9,59 +9,59 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.meyou.app.R
 import com.meyou.app.detailDish.tap1.DetailActivity
-import com.meyou.app.detailDish.tap3.VisiteCatActivity
+import com.meyou.app.detailDish.tap1.DetailDIshInfo
+import com.meyou.app.detailDish.tap2.DIshCat
+import com.meyou.app.detailDish.tap2.DishCatAdapter
 
-class DIshCat : AppCompatActivity() {
-    private var catList = mutableListOf<DIshCatInfo>()
+class VisiteCatActivity : AppCompatActivity() {
+    private var dishList = mutableListOf<VisiteCatInfo>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        catList.add(
-            DIshCatInfo(
+        dishList.add(
+            VisiteCatInfo(
                 "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRUiNRqrAJKbWIZ-1Nl8tygN61EqmpRuveYzQ&usqp=CAU",
-                "장군이네",
-                true,
-                "19:00~21:00"
+                "2023-05-01 15:30",
             )
         )
-        catList.add(
-            DIshCatInfo(
+        dishList.add(
+            VisiteCatInfo(
                 "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRUiNRqrAJKbWIZ-1Nl8tygN61EqmpRuveYzQ&usqp=CAU",
-                "장군이네",
-                true,
-                "19:00~21:00"
+                "2023-05-01 15:30",
             )
         )
-        catList.add(
-            DIshCatInfo(
+        dishList.add(
+            VisiteCatInfo(
                 "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRUiNRqrAJKbWIZ-1Nl8tygN61EqmpRuveYzQ&usqp=CAU",
-                "장군이네",
-                false,
-                "19:00~21:00"
+                "2023-05-01 15:30",
             )
         )
-
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_tap2_dish_cat)
+        setContentView(R.layout.activity_tap3_visite_cat)
+
 
         // RecyclerView 초기화
         val dishNewData = findViewById<RecyclerView>(R.id.rvdish)
         dishNewData.layoutManager = LinearLayoutManager(this)
 
         // Adapter 설정
-        val dishNewAdapter = DishCatAdapter(this, catList)
+        val dishNewAdapter = VisiteCatAdapter(this, dishList)
         dishNewData.adapter = dishNewAdapter
         dishNewData.layoutManager = GridLayoutManager(this, 2)
 
 
+
+
+        // 상단 탭 클릭시 액티비티 이동
         val tap1 = findViewById<TextView>(R.id.tap1)
         tap1.setOnClickListener {
             val intent = Intent(this, DetailActivity::class.java)
             startActivity(intent)
         }
-        val tap3 = findViewById<TextView>(R.id.tap3)
-        tap3.setOnClickListener {
-            val intent = Intent(this, VisiteCatActivity::class.java)
+        val tap2 = findViewById<TextView>(R.id.tap2)
+        tap2.setOnClickListener {
+            val intent = Intent(this, DIshCat::class.java)
             startActivity(intent)
         }
+
     }
 }
