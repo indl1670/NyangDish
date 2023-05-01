6da12908d0cb8ab9822a8e96e52e71d2f1b53fec
baebug <ufr134@gmail.com>
2023-05-01 16:23:18 +0900

    🦕 DB 공백 문자열 오류 수정

--- ourkitty/src/main/kotlin/com/nyang/ourkitty/entity/ClientEntity.kt
+++ ourkitty/src/main/kotlin/com/nyang/ourkitty/entity/ClientEntity.kt
@@ -14,12 +14,16 @@ class ClientEntity(
     val clientProfileImagePath: String,
     val clientAddress: String,
     val clientPhone: String,
-    val userCode: String = UserCode.캣맘.code,
     val locationCode: String,
-    val lastPostingDate: LocalDateTime = LocalDateTime.of(1970, 1, 1, 0, 0, 0),
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     val clientId: Long? = null,
+
+    @OneToMany(mappedBy = "client", cascade = [CascadeType.PERSIST, CascadeType.MERGE])
+    val dishList: MutableList<ClientDishEntity> = mutableListOf(),
+
+    val userCode: String = UserCode.캣맘.code,
+    val lastPostingDate: LocalDateTime = LocalDateTime.of(2000, 1, 1, 0, 0, 0),
 ) : BaseEntity() {
 
     //TODO : updateDish or addDish -> DishClientEntity 생성하는 로직
