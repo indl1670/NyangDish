b0e509a59503843eb70b8c661740aea491455e49
강동훈 <asddiin@gmail.com>
2023-05-02 17:02:29 +0900

    🎮배터리 잔량체크 개발 - 배터리 잔량체크 후 결과값 서버로 전송하는 코드. 냥그릇 공통코드를   반영했고, 50000mA 대룡량 보조배터리 기준으로 설정값을 지정했습니다. S08P31E203-266

--- iot/feature/bettery_check/bettery_check.ino
+++ iot/feature/bettery_check/bettery_check.ino
@@ -2,10 +2,11 @@
 
 // 배터리 충전삳태 상수. 0V ~ 5V 의 값을 1024단계로 나누어 단계를 표현
 // 특정 전압 이상일 때 표시가 바뀌는 형식
+// 아래 수치는 50000mA 배터리 기준
 #define FULL        1000
 #define SAFE        950
-#define WARNING     900
-#define DANGER      850
+#define WARNING     850
+#define DANGER      750
 // ===========================
 // Enter your WiFi credentials
 // ===========================
@@ -44,31 +45,41 @@ void setup() {
 }
 
 void loop() {
-  int sensorValue = analogRead(A0);
-  uint8_t code = 0;
+  int sum, avg;
+  uint8_t count = 0;
+  // 20회 측정 후 평균치 사용
+  while(count < 20) {
+    int sensorValue = analogRead(A0);
+    sum += sensorValue ;
+    delay(100);
+    count++;
+  }
+  avg = sum / count;
+  // 평균값에 맞는 전송수치 보내기
+  String code;
   Serial.println(sensorValue);
   // 배터리 완충
-  if (sensorValue > FULL) {
-    code = 0;
+  if (avg > FULL) {
+    code = "0100001";
   }
   // 배터리 안정적
-  else if (sensorValue > SAFE) {
-    code = 1;
-  }
-  // 배터리 위혐
-  else if (sensorValue > WARNING) {
-    code = 2;
+  else if (avg > SAFE) {
+    code = "0100002";
   }
   // 배터리 경고
+  else if (avg > WARNING) {
+    code = "0100003";
+  }
+  // 배터리 위험
   else {
-    code = 3;
+    code = "0100004";
   }
 
-  sendData(sensorValue)
+  sendData(code)
   delay(1000);
 }
 
-String sendData(uint8_t sensor_data) {
+String sendData(String sensor_data) {
   String getAll;
   String getBody;
 
@@ -76,7 +87,7 @@ String sendData(uint8_t sensor_data) {
 
   if (client.connect(serverName.c_str(), serverPort)) {
     Serial.println("Connection successful!");    
-    String body = "{\"dishSerialNum\": \"" + serialNumber + "\", \"dishBettery\": " + String(sensor_data) +"}";
+    String body = "{\"dishSerialNum\": \"" + serialNumber + "\", \"dishBettery\": \"" + sensor_data +"\"}";
 
     uint32_t totalLen = body.length();
   
