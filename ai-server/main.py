6ebb4ec49ab9c55c63ac1cb807f25a484ef8e934
송주영 <juyeong0206@gmail.com>
2023-05-04 17:17:30 +0900

    🥨S3 Upload, Cat Detection Model Upgraded, Display pictures by time (10minutes) - S3 Upload 기능 - Cat Detection Model 고도화(9000장 학습) - 사진 보여줄 때 현재 기준으로 10분 안에 찍힌 사진 display #S08P31E203-255 #S08P31E203-258

--- ai-server/main.py
+++ ai-server/main.py
@@ -1,12 +1,12 @@
 import os.path
-import glob
+import datetime
+from glob import glob
 
 # FastAPI에서 CORSMiddleware라는 모듈로 CORS를 제어한다.
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI, UploadFile, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
-from datetime import datetime
 import asyncio
 # Google
 from google_drive import connect_to_google_drive, upload_photo
@@ -14,6 +14,8 @@ from dotenv import load_dotenv
 from detr import detectCatByDetr
 # YOLO
 from yolov5.yolo import detectCatByYolo
+# s3
+from s3upload import upload_image
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 load_dotenv(os.path.join(BASE_DIR, ".env"))
@@ -58,7 +60,7 @@ async def upload_google_model_yolo_detr(serial_number, imageFile: UploadFile or 
     # 이미지 파일 이름 설정
     fileName = os.environ[serial_number+"_NAME"]
     filePath = "static/img/"+fileName+".png"
-    googleFileName = datetime.today().strftime("%Y-%m-%d_%H-%M-%S")
+    googleFileName = datetime.datetime.today().strftime("%Y-%m-%d_%H-%M-%S")
     googleFileName = fileName+"_"+googleFileName
 
     # 읽은 파일을 구글에 보내기 위해 서버에 저장
@@ -78,6 +80,10 @@ async def upload_google_model_yolo_detr(serial_number, imageFile: UploadFile or 
     else:
         return {'status': 200, 'message': "고양이 사진이 아닙니다."}
 
+@app.post("/upload-s3/{serial_number}")
+async def upload_s3(serial_number, imageFile: UploadFile or None = None):
+    return upload_image(serial_number, imageFile)
+
 @app.get("/yolo/pics")
 def display_yolo_pics():
     return display_pics("yolo")
@@ -91,16 +97,24 @@ def display_detr_pics():
     return display_pics("img")
 
 def display_pics(folderName):
-    # 이미지 파일의 확장자 리스트
-    img_extensions = ['.jpg', '.jpeg', '.png', '.gif']
-    # 가져올 이미지 파일이 있는 폴더 경로
-    dir_path = f"./static/{folderName}"
-    # 폴더 안의 모든 파일을 가져와서 정렬
-    image_list = sorted([f for f in os.listdir(dir_path) if os.path.splitext(f)[-1].lower() in img_extensions], key=lambda x: os.path.getmtime(os.path.join(dir_path, x)), reverse=True)
+    img_list = [f for f in os.listdir(f"./static/{folderName}") if f.endswith(".png")]
+    values = []
+    for i in img_list:
+        date = i.split('.')[0].split('_')
+        values.append(date)
+
+    now = datetime.datetime.now()
+    delta = datetime.timedelta(minutes=10)
+    before = now - delta
+
+    filtered_values = [value for value in values if datetime.datetime.strptime(f"{value[1]} {value[2]}", "%Y-%m-%d %H-%M-%S") >= before]
+
+    image_list = []
+    for i in filtered_values:
+        image_list.append("_".join(i))
     html_content = "<html><body><h3>최신순 10개</h1><div style='display: flex; gap: 10px; flex-wrap: wrap;'>"
-    for image in image_list[:10]:
-        if image.endswith(".jpg") or image.endswith(".png"):
-            html_content += f'<div><div>{image}</div><img src="/static/{folderName}/{image}" alt="{image}" width="416" ></div>'
+    for image in image_list:
+        html_content += f'<div><div>{image}</div><img src="/static/{folderName}/{image}.png" alt="{image}" width="416" ></div>'
     html_content += "</div></body></html>"
     return HTMLResponse(content=html_content, status_code=200)
 
---
+++ ai-server/s3upload.py
@@ -0,0 +1,21 @@
+import os
+import boto3
+import uuid
+from botocore.client import Config
+from fastapi import UploadFile
+
+BUCKET_NAME = os.environ['BUCKET_NAME']
+S3_REGION = os.environ['S3_REGION']
+
+s3 = boto3.client('s3')
+
+def upload_image(serial_number, file: UploadFile):
+  file_extension = file.filename.split('.')[-1]
+  directory = os.environ[f'{serial_number}_S3']
+  key = f"{directory}/{uuid.uuid4()}.{file_extension}"
+  ExtraArgs = {
+    'ContentType': file.content_type,
+    'ContentDisposition': 'inline',
+  }
+  s3.upload_fileobj(file.file, BUCKET_NAME, key, ExtraArgs=ExtraArgs)
+  return f"https://{BUCKET_NAME}.s3.{S3_REGION}.amazonaws.com/{key}"
