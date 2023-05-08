5cfeab2c24a2e6e74071b6671883a844d6f93060
baebug <ufr134@gmail.com>
2023-05-08 22:35:27 +0900

    🦕 차트 API - 히트맵 데이터 - AI server getImage API  #S08P31E203-319

--- ourkitty/src/main/resources/db/data.sql
+++ ourkitty/src/main/resources/db/data.sql
@@ -1,33 +1,33 @@
 INSERT INTO `client_table`
 VALUES (1, 'client', '$2a$10$kNnSCr0rS0SxvR7w09F2FetmPh6doUi7GyrZAXsQ3BOSFEbY5YLha', 'name1', '테스트유저', '', 'address1',
-        '01000000001', '0010001', '0020001',
+        '010-0000-0001', '0010001', '0020001',
         now(),
         '0110001', false, '',
         now(), now()),
        (2, 'admin', '$2a$10$kNnSCr0rS0SxvR7w09F2FetmPh6doUi7GyrZAXsQ3BOSFEbY5YLha', 'name2', '테스트관리자', '', 'address2',
-        '01000000002', '0010002', '0020001',
+        '010-0000-0002', '0010002', '0020001',
         now(),
         '0110001', false, '',
         now(), now()),
        (3, 'test3@ssafy.com', '$2a$10$kNnSCr0rS0SxvR7w09F2FetmPh6doUi7GyrZAXsQ3BOSFEbY5YLha', 'name3', 'nickname3', '',
-        'address3', '01000000003', '0010001', '0020001',
+        'address3', '010-0000-0003', '0010001', '0020001',
         now(),
         '0110001', false, '',
         now(), now()),
        (4, 'test4@ssafy.com', '$2a$10$kNnSCr0rS0SxvR7w09F2FetmPh6doUi7GyrZAXsQ3BOSFEbY5YLha', 'name4', 'nickname3', '',
-        'address3', '01000000004', '0010001', '0020001',
+        'address3', '010-0000-0004', '0010001', '0020001',
         now(),
         '0110003', false, '이사',
         now(), now()),
        (5, 'consultant@ssafy.com', '$2a$10$URlnKnYUThXPyQbFjQzf4eVrQSLNvY19AyICcCJsHAQ.jQRJ/Q21e', 'consultant', '컨설턴트',
         '', 'ssafy 202',
-        '01022222222', '0010002', '0020001',
+        '010-2222-2222', '0010002', '0020001',
         now(),
         '0110001', false, '',
         now(), now()),
        (6, 'coach@ssafy.com', '$2a$10$CYNtFi7GH/RFohNii2J7neI5SFghBdOEcUuA9fPrQHK22IOltDhfi', 'coach', '코치', '',
         'ssafy 202',
-        '01033333333', '0010002', '0020001',
+        '010-3333-3333', '0010002', '0020001',
         now(),
         '0110001', false, '',
         now(), now());
@@ -139,8 +139,8 @@ VALUES (1, 1, '2023-05-08', 5, 4, false, now(), now()),
        (4, 1, '2023-05-03', 6, 1, false, now(), now()),
        (5, 1, '2023-05-02', 5, 3, false, now(), now()),
        (6, 1, '2023-05-04', 5, 3, false, now(), now()),
-       -- (7, 1, '2023-05-07', 6, 4, false, now(), now()),
-       -- (8, 1, '2023-05-06', 5, 3, false, now(), now()),
+       (7, 1, '2023-05-07', 6, 4, false, now(), now()),
+       (8, 1, '2023-05-06', 5, 3, false, now(), now()),
        (9, 1, '2023-04-30', 6, 2, false, now(), now());
 
 INSERT INTO `management_table`
@@ -157,11 +157,11 @@ VALUES (1, 1, 1, '고양이 커여웡', '0030001', '0020001', false, now(), now(
 INSERT INTO `management_comment_table`
 VALUES (1, 1, 1, '고양이 짱커여웡', false, now(), now());
 
--- INSERT INTO `management_image_table`
--- VALUES (1, 1, './default.png', false, now(), now()),
---        (2, 2, './default.png', false, now(), now()),
---        (3, 2, './default.png', false, now(), now()),
---        (4, 3, './default.png', false, now(), now());
+INSERT INTO `management_image_table`
+VALUES (1, 1, 'https://nyang-s3.s3.ap-northeast-2.amazonaws.com/animals.png', false, now(), now()),
+       (2, 2, 'https://nyang-s3.s3.ap-northeast-2.amazonaws.com/animals.png', false, now(), now()),
+       (3, 2, 'https://nyang-s3.s3.ap-northeast-2.amazonaws.com/animals.png', false, now(), now()),
+       (4, 3, 'https://nyang-s3.s3.ap-northeast-2.amazonaws.com/animals.png', false, now(), now());
 
 INSERT INTO `report_table`
 VALUES (1, 1, 1, '신고1', '0040001', '고양이 아파', '처방전1', '0020001', '0050001', false, now(), now()),
