5ceaa2589dc35f4b77fb03fe3507362c40c25e06
이미현 <algus7531@naver.com>
2023-05-09 09:17:57 +0900

    🌴관리일지 상세, 신고 작성 페이지

--- android/app/src/main/java/com/meyou/app/management/ManagementFragment.kt
+++ android/app/src/main/java/com/meyou/app/management/ManagementFragment.kt
@@ -1,5 +1,6 @@
 package com.meyou.app.management
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -8,11 +9,11 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.ImageView
 import android.widget.Spinner
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.meyou.app.R
-import com.meyou.app.main.ContentsMyDishList
 
 class ManagementFragment : Fragment() {
     private var dataList = mutableListOf<ManagementInfo>()
@@ -28,14 +29,17 @@ class ManagementFragment : Fragment() {
 
         val spinner = view.findViewById<Spinner>(R.id.home_spinner)
 
-        // 데이터 배열을 만듭니다.
+// 데이터 배열을 만듭니다.
         val data = arrayOf("전체","아이유정", "미현이네", "정호네")
 
-        // ArrayAdapter를 생성합니다.
+// ArrayAdapter를 생성합니다.
         val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_dropdown_item, data)
 
-        // 생성한 ArrayAdapter를 Spinner에 설정합니다.
+// 생성한 ArrayAdapter를 Spinner에 설정합니다.
         spinner.adapter = adapter
+
+// 기본 선택 상태를 설정하지 않음
+        spinner.setSelection(0)
 
         spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onItemSelected(
@@ -44,15 +48,16 @@ class ManagementFragment : Fragment() {
                 position: Int,
                 id: Long
             ) {
-                val selectedItem = parent?.getItemAtPosition(position)
-                Log.d("Spinner", "Selected Item: $selectedItem")
+                if (position >= 0) { // -1이 아닌 경우에만 선택 이벤트를 처리
+                    val selectedItem = parent?.getItemAtPosition(position)
+                    Log.d("Spinner", "Selected Item: $selectedItem")
+                }
             }
 
             override fun onNothingSelected(parent: AdapterView<*>?) {
                 Log.d("Spinner", "Nothing Selected")
             }
         }
-
         dataList.add(
             ManagementInfo(
                 listOf("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRUiNRqrAJKbWIZ-1Nl8tygN61EqmpRuveYzQ&usqp=CAU","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRUiNRqrAJKbWIZ-1Nl8tygN61EqmpRuveYzQ&usqp=CAU"),
@@ -81,6 +86,13 @@ class ManagementFragment : Fragment() {
         val manageAdapter = ManagementAdapter(requireContext(), dataList)
         ManageData.adapter = manageAdapter
         ManageData.layoutManager = LinearLayoutManager(requireContext())
+
+        // 클릭시 CreageManagementActivity 로 이동
+        val createFormImageView: ImageView = view.findViewById(R.id.create_form)
+        createFormImageView.setOnClickListener {
+            val intent = Intent(requireContext(), CreateManagementActivity::class.java)
+            startActivity(intent)
+        }
 
         return view
     }
