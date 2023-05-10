1d9cbb3bab2eda71247a94e02191b16e9a311dfc
송주영 <juyeong0206@gmail.com>
2023-05-10 12:20:01 +0900

    fe 작업
    Former-commit-id: 20f5ec7d72308b94bea4e70c59d62298b1d2d912

--- frontend/src/pages/DishDetail.tsx
+++ frontend/src/pages/DishDetail.tsx
@@ -1,27 +1,27 @@
 import React from "react";
 import CatButton from "../components/chart/CatButton";
 import ClusteringChart from "../components/dishDetail/ClusteringChart";
-import DateButton from "../components/dishDetail/DateButton";
-import ResultImage from './../components/dishDetail/ResultImage';
+import DateList from "../components/dishDetail/DateList";
+import ClusteringResult from '../components/dishDetail/ClusteringResult';
 
 export default function DishDetail() {
   return (
     <div className="w-full h-full flex flex-row gap-[15px] p-2">
       <div className="w-[16%] h-full flex flex-col gap-2">
         <div className="w-full h-full bg-white p-3 rounded-lg dark:bg-DarkBackground2 dark:text-white">
-          <CatButton/>
+          <CatButton />
         </div>
       </div>
       <div className="w-[84%] h-full flex flex-col gap-2">
-        <div className="w-full h-[20%] bg-white p-3 rounded-lg dark:bg-DarkBackground2 dark:text-white">
-          <DateButton/>
+        <div className="w-full h-[60px] bg-white p-3 rounded-lg dark:bg-DarkBackground2 dark:text-white">
+          <DateList />
         </div>
-        <div className="w-full h-[80%] flex flex-row gap-2">
+        <div className="w-full h-[calc(100%-60px)] flex flex-row gap-2">
           <div className="w-[70%] h-full bg-white p-3 rounded-lg dark:bg-DarkBackground2 dark:text-white">
-            <ClusteringChart/>
+            <ClusteringChart />
           </div>
           <div className="w-[30%] h-full bg-white p-3 rounded-lg dark:bg-DarkBackground2 dark:text-white">
-            <ResultImage/>
+            <ClusteringResult />
           </div>
         </div>
       </div>
