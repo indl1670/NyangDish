c73d66eec529f1c05b1e101f9261d36ae007d71c
Yun <indl1670@gmail.com>
2023-05-17 16:04:40 +0900

    🎈NotFound Page #S08P31E203-383
    Former-commit-id: a20de19821b6646f4fa84074ec7d89d2e4a98a19

--- frontend/src/components/common/NotFound.tsx
+++ frontend/src/components/common/NotFound.tsx
@@ -1,5 +1,21 @@
 import React from "react";
-
+import LightNotFound from "../../assets/light_notfound.gif";
+import DarkNotFound from "../../assets/dark_notfound.gif";
+import { useRecoilState } from "recoil";
+import { darkState } from "recoil/page";
 export default function NotFound() {
-  return <div>NotFound</div>;
+  const isDark = useRecoilState(darkState)[0];
+
+  return (
+    <div className="w-full h-full flex flex-col dark:bg-DarkBackground2">
+      <img
+        src={isDark ? DarkNotFound : LightNotFound}
+        alt=""
+        className="m-auto"
+      />
+      <div className="m-auto text-[2rem] font-bold dark:text-white">
+        요청하신 페이지를 찾을 수 없습니다.
+      </div>
+    </div>
+  );
 }
